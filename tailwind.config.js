f0311ad790af4661d38c3fb9e4ba06100dbf88e2
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-05-11 01:08:16 +0300

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        "gradient-xy": "gradient-xy 20s ease-in-out infinite",
+        "gradient-xy-fast": "gradient-xy 25s ease-in-out infinite",
+        "gradient-xy-faster": "gradient-xy 30s ease-in-out infinite",
+      },
+      keyframes: {
+        "gradient-xy": {
+          "0%, 100%": {
+            "background-size": "700% 700%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "700% 700%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
