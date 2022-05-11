f57e7dfc9b0f492e3dd6ef0e9a0295c969fb7a7d
William Gitta <73100027+KabakaWilliam@users.noreply.github.com>
2022-05-11 14:58:49 +0300

    added mobile cards view

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -4,11 +4,11 @@ const ArticleCard = () => {
     <div className="w-[100%] h-max flex-col flex items-center justify-center border-y border-[1px] border-[#323334]">
       <div className="w-[100%] h-[300px] flex items-center justify-center  ">
         {/* actual card */}
-        <div className="w-[60%] h-[240px]  rounded-lg z-30 bg-green-400 group cursor-pointer  ">
+        <div className=" w-[300px] md:w-[60%] h-[240px]  rounded-lg z-30 bg-green-400 group cursor-pointer  ">
           <div className="w-[100%] h-[240px] border rounded-lg z-30 p-5 bg-black transition ease-in-out delay-150  group-hover:relative  group-hover:translate-y-5 group-hover:translate-x-10 ">
             {" "}
             {/*hover:border-[#EC519B]*/}
-            <div className="w-[100%] h-[50%] flex  items-center justify-center text-white text-4xl font-bold">
+            <div className="w-[100%] h-[50%] flex  items-center justify-center text-white text-3xl md:text-4xl font-bold">
               Why I dont learn CLI commands anymore ?
             </div>
             <div className="w-[100%] h-[50%] flex items-end text-white ">
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,8 @@ const Home: NextPage = () => {
       </Head>
       <div>
         <Navbar />
-        <div className="w-[60vw] h-max mx-[20vw] mt-[10vh] border-dashed border-4 ">
-          <div className="w-[100%] select-none h-[15vh]  text-[100px] font-bold textGradientWelcome animate-gradient-xy ">
+        <div className="w-[100vw] md:w-[60vw] h-max md:mx-[20vw] mt-[10vh] border-dashed border-4 ">
+          <div className="w-[100%] select-none h-[15vh] text-[60px] flex items-center md:block  md:text-[100px] font-bold textGradientWelcome animate-gradient-xy ">
             {"Will's Blog"}
           </div>
           <div className="w-[100%] h-max  flex items-center justify-center">
